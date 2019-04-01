5d135c0d100b742ce1fb9bb73dbb3efaa76cf4b9
Javier Cabrera Arteaga <xppcoder@gmail.com>
2019-04-01 16:09:10 +0200

    WIP

--- src/main/kotlin/astminer/examples/AllJsFiles.kt
+++ src/main/kotlin/astminer/examples/AllJsFiles.kt
@@ -9,16 +9,28 @@ import java.io.File
 
 
 fun allJsFiles() {
-    val folder = "./testData/examples/"
+    val folder = "./testData"
 
     val miner = PathMiner(PathRetrievalSettings(5, 5))
     val storage = VocabularyPathStorage()
 
     File(folder).walkTopDown().filter { it.path.endsWith(".js") }.forEach { file ->
-        val node = ECMAParser().parse(file.inputStream()) ?: return@forEach
-        val paths = miner.retrievePaths(node)
 
-        storage.store(paths.map { toPathContext(it) }, entityId = file.path)
+        try{
+
+            print("Parsing..." + file)
+            print("\n")
+
+
+            val node = ECMAParser().parse(file.inputStream()) ?: return@forEach
+            val paths = miner.retrievePaths(node)
+
+            storage.store(paths.map { toPathContext(it) }, entityId = file.path)
+        }
+        catch (e: Exception){
+            print(e.message)
+            print("\n")
+        }
     }
 
     storage.save("out_examples/allJSFiles")
